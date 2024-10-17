672bb04025bc931c23cc6b655c441bf384b5b02a
Chirayu Shah <chirayushah61@gmail.com>
2024-10-17 13:47:31 -0600

    Added firebase stuff

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
+//    id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -35,7 +37,7 @@ android {
 }
 
 dependencies {
-
+    implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
