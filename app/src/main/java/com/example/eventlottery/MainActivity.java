4dfb0456ce732c41dc4843ac93cd49287ff400c3
Chirayu Shah <chirayushah61@gmail.com>
2024-10-18 01:01:47 -0600

    Adding basic user details

--- app/src/main/java/com/example/eventlottery/MainActivity.java
+++ app/src/main/java/com/example/eventlottery/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.eventlottery;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -15,11 +17,19 @@ import com.example.eventlottery.databinding.ActivityMainBinding;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.UUID;
 
+import android.provider.Settings;
+import android.util.Base64;
 import android.util.Log;
+import android.os.FileUtils;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseFirestore db;
     private CollectionReference userRef;
+    private CollectionReference photosRef;
     protected String androidID;
 
 
@@ -48,29 +59,29 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-        try {
-            androidID = createTransactionID();
-        } catch (Exception e) {
-            Log.e("Error", e.toString());
-        }
+        androidID = getAndroidID();
 
         db = FirebaseFirestore.getInstance();
         userRef = db.collection("users");
+        photosRef = db.collection("photos");
 
         HashMap<String, String> data = new HashMap<>();
         data.put("android_id", androidID);
+        data.put("f_name", "Chirayu");
+        data.put("l_name", "Shah");
+        data.put("email", "cshah1@ualberta.ca");
         userRef.document(androidID).set(data).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void unused) {
                 Log.d("Firestore", "Document successfully written");
             }
         });
-
     }
     // The following function is from:
-    // https://stackoverflow.com/questions/40237169/generating-unique-id-in-android-uuid October 17 2024
-    public String createTransactionID() throws Exception{
-        return UUID.randomUUID().toString().replaceAll("-", "").toUpperCase();
+    private String getAndroidID(){
+        // https://www.geeksforgeeks.org/how-to-fetch-device-id-in-android-programmatically/
+        return Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
     }
+
 
 }
