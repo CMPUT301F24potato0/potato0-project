f558ebbf362be5e19901eeec84e3ff582d66831c
Chirayu Shah <chirayushah61@gmail.com>
2024-10-19 15:14:12 -0600

    Errors in main activity

--- app/src/main/java/com/example/eventlottery/MainActivity.java
+++ app/src/main/java/com/example/eventlottery/MainActivity.java
@@ -1,89 +1,86 @@
 package com.example.eventlottery;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
-import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.WriterException;
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.journeyapps.barcodescanner.BarcodeEncoder;
-import com.journeyapps.barcodescanner.BarcodeView;
-import com.journeyapps.barcodescanner.DecoratedBarcodeView;
-import com.journeyapps.barcodescanner.ScanContract;
-import com.journeyapps.barcodescanner.ScanOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        // Check this for Qr scanner in fragment
-        // https://stackoverflow.com/questions/40725336/android-studio-start-qr-code-scanner-from-fragment
-        // https://medium.com/@dev.jeevanyohan/zxing-qr-code-scanner-android-implementing-in-activities-fragment-custom-colors-faa68bfc761d
 
-//        // https://github.com/journeyapps/zxing-android-embedded/tree/master
-//        ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
-//                result -> {
-//                    if (result.getContents() == null) {
-//                        Toast.makeText(MainActivity.this, "Cancelled", Toast.LENGTH_LONG).show();
-//                    }
-//                    else {
-//                        Toast.makeText(MainActivity.this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
-//                    }
-//                });
+        bottomNavigationView
+                = findViewById(R.id.bottomNavigationView);
 
-//        Button scan_button = findViewById(R.id.scan_button);
-//        scan_button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                ScanOptions options = new ScanOptions();
-//                options.setOrientationLocked(false);
-//                options.setBeepEnabled(false);
-//                options.setDesiredBarcodeFormats(ScanOptions.QR_CODE);
-//                barcodeLauncher.launch(options);
-//            }
+        bottomNavigationView
+                .setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+//        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
 //        });
-        new ScanContract();
-        new ScanOptions();
+    }
 
-        DecoratedBarcodeView barcodeView = findViewById(R.id.scanner);
-        QR_Handler qrHandler = new QR_Handler(barcodeView);
+    ScanFragment scanFragment = new ScanFragment();
+    NotificationsFragment notificationsFragment = new NotificationsFragment();
+    FacilityFragment facilityFragment = new FacilityFragment();
+    WaitlistedEventsFragment waitlistedEventsFragment = new WaitlistedEventsFragment();
+    Profile profileFragment = new Profile();
 
-        Button scan_button = findViewById(R.id.scan_button);
-        scan_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //TODO: Implement this after implementing QR_Scan_Fragment and QR_Handler
-                Toast.makeText(MainActivity.this, "Implement this button", Toast.LENGTH_SHORT).show();
-            }
-        });
+    @Override
+    public boolean
+    onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.scanQR:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, scanFragment)
+                        .commit();
+                return true;
 
-        Button generate_button = findViewById(R.id.generate_button);
-        generate_button.setOnClickListener(( new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    Bitmap bitmap = qrHandler.generate_QR_Bitmap("Hello World!");
-                    ImageView qrCode = findViewById(R.id.qrCode);
-                    qrCode.setImageBitmap(bitmap);
-                } catch (WriterException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }));
+            case R.id.notifications:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, notificationsFragment)
+                        .commit();
+                return true;
+
+            case R.id.facility:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, facilityFragment)
+                        .commit();
+                return true;
+            case R.id.waitlist:
+                getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, waitlistedEventsFragment)
+                    .commit();
+                return true;
+
+            case R.id.profile:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, profileFragment)
+                        .commit();
+                return true;
+        }
+        return false;
     }
 }
