a990e8f74bc2b57e6262b2a3fecbd9015cdd0163
carrandez <vagundez1@gmail.com>
2024-10-21 12:27:52 -0600

    Add user profile functionality now the user can provide data and edit it anytime

--- app/src/main/java/com/example/eventlottery/Profile.java
+++ app/src/main/java/com/example/eventlottery/Profile.java
@@ -2,18 +2,31 @@ package com.example.eventlottery;
 
 
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class Profile extends Fragment {
 
     private CurrentUser curUser;
     private FirebaseFirestore db;
+    public CollectionReference userRef;
+
+    private Button editUser;
+
+
     public Profile(){
         // require a empty public constructor
     }
@@ -30,9 +43,50 @@ public class Profile extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        //return inflater.inflate(R.layout.fragment_profile, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        db = FirebaseFirestore.getInstance();
+        userRef = db.collection("users");
 
 
+        EditText f_name = (EditText) rootView.findViewById(R.id.fNameEditText);
+        EditText l_name = (EditText) rootView.findViewById(R.id.lNameEditText);
+        EditText email = (EditText) rootView.findViewById(R.id.emailEditText);
+        EditText phone = (EditText) rootView.findViewById(R.id.phoneEditText);
+
+        f_name.setText(curUser.getfName());
+        l_name.setText(curUser.getlName());
+        email.setText(curUser.getEmail());
+        phone.setText(curUser.getPhone());
+
+        editUser = (Button) rootView.findViewById(R.id.saveProfileBtn);
+        editUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String f_nameStr = f_name.getText().toString();
+                String l_nameStr = l_name.getText().toString();
+                String emailStr = email.getText().toString();
+                String phoneStr = phone.getText().toString();
+
+                curUser.setfName(f_nameStr);
+                curUser.setlName(l_nameStr);
+                curUser.setEmail(emailStr);
+                curUser.setPhone(phoneStr);
+
+                HashMap<String, String> data = new HashMap<>();
+                data.put("android_id", curUser.getiD());
+                data.put("email", curUser.getEmail());
+                data.put("f_name", curUser.getfName());
+                data.put("l_name", curUser.getlName());
+
+                userRef.document(curUser.getiD()).set(data);
+
+            }
+        });
+
+        return rootView;
     }
 }
 //
