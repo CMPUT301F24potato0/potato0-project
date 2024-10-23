ef6f010c0b149118a7d0b52cfbb9835a9d10a547
Chirayu Shah <chirayushah61@gmail.com>
2024-10-23 00:36:27 -0600

    Added JavaDocs and cleaned up the code

--- app/src/main/java/com/example/eventlottery/Profile.java
+++ app/src/main/java/com/example/eventlottery/Profile.java
@@ -2,15 +2,12 @@ package com.example.eventlottery;
 
 
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.firestore.CollectionReference;
@@ -18,6 +15,10 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 
+/**
+ * This class is the profile fragment.
+ * This class is called when the user clicks the profile button on the bottom.
+ */
 public class Profile extends Fragment {
 
     private CurrentUser curUser;
@@ -26,20 +27,36 @@ public class Profile extends Fragment {
 
     private Button editUser;
 
-
+    /**
+     * Empty Constructor
+     */
     public Profile(){
         // require a empty public constructor
     }
 
-    public Profile(FirebaseFirestore db) {
-        this.db = db;
-    }
-
+    /**
+     * This constructor is used to pass in the instance of CurrentUser
+     * @param db This is the database instance
+     * @param curUser This is the information about current user which is passed from MainActivity
+     */
     public Profile(FirebaseFirestore db, CurrentUser curUser) {
         this.db = db;
         this.curUser = curUser;
     }
 
+    /**
+     * This method is called when creating the fragment.
+     * This method uses the CurrentUser instance and puts the information if any in the EditText in the fragment.
+     * @param inflater The LayoutInflater object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI should be attached to.  The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     *
+     * @return returns the View
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
@@ -89,66 +106,3 @@ public class Profile extends Fragment {
         return rootView;
     }
 }
-//
-//import android.os.Bundle;
-//
-//import androidx.fragment.app.Fragment;
-//
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//
-///**
-// * A simple {@link Fragment} subclass.
-// * Use the {@link Profile#newInstance} factory method to
-// * create an instance of this fragment.
-// */
-//public class Profile extends Fragment {
-//
-//    // TODO: Rename parameter arguments, choose names that match
-//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
-//
-//    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
-//
-//    public Profile() {
-//        // Required empty public constructor
-//    }
-//
-//    /**
-//     * Use this factory method to create a new instance of
-//     * this fragment using the provided parameters.
-//     *
-//     * @param param1 Parameter 1.
-//     * @param param2 Parameter 2.
-//     * @return A new instance of fragment Profile.
-//     */
-//    // TODO: Rename and change types and number of parameters
-//    public static Profile newInstance(String param1, String param2) {
-//        Profile fragment = new Profile();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//    }
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_profile, container, false);
-//    }
-//}
