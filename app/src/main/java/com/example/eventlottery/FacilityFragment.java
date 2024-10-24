7a43feb1554798d6b802eb5d72143e5d38e5a8d2
EdwardGallardo <gallardoedwardjason@gmail.com>
2024-10-23 22:49:02 -0600

    Finished facility page UI

--- app/src/main/java/com/example/eventlottery/FacilityFragment.java
+++ app/src/main/java/com/example/eventlottery/FacilityFragment.java
@@ -4,8 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * This class is the facility fragment
@@ -33,6 +38,8 @@ public class FacilityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_facility, container, false);
+        View rootview = inflater.inflate(R.layout.fragment_facility, container, false);
+
+        return rootview;
     }
 }
