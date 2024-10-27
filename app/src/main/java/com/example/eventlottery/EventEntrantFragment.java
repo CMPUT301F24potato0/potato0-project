9df6c88c83da06a4a4ecbf7efc8694a290a490e1
EdwardGallardo <gallardoedwardjason@gmail.com>
2024-10-27 00:00:13 -0600

    Finished entrant UI on event page

---
+++ app/src/main/java/com/example/eventlottery/EventEntrantFragment.java
@@ -0,0 +1,23 @@
+package com.example.eventlottery;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class EventEntrantFragment extends Fragment {
+
+    public EventEntrantFragment() {
+        super();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_event_entrant, container, false);
+        return rootView;
+    }
+}
