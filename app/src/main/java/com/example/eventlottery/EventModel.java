b798ae7f00c7825aacb42091317e45fb95defc30
EdwardGallardo <gallardoedwardjason@gmail.com>
2024-10-30 20:58:41 -0600

    Finished waiting list join and unjoin functions in Event Model class, and added templates for other lists

--- app/src/main/java/com/example/eventlottery/EventModel.java
+++ app/src/main/java/com/example/eventlottery/EventModel.java
@@ -1,6 +1,8 @@
 package com.example.eventlottery;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Random;
 
 public class EventModel {
     private String facilityID;
@@ -11,104 +13,106 @@ public class EventModel {
     private ArrayList<String> cancelledList;
     private ArrayList<String> enrolledList;
     private Boolean geolocationRequired;
+    private Integer waitingListLimit;
     private Integer capacity;
+    private Date joinDeadline;
+    private String eventStrLocation;
+    private String eventTitle;
     private String hashQR; // TODO: part 4
     // TODO: part 4 - poster image
 
-    public EventModel() {
-
-    }
-
-    public EventModel(String facilityID, String eventID,
-                      ArrayList<String> waitingList,
-                      ArrayList<String> invitedList,
-                      ArrayList<String> cancelledList,
-                      ArrayList<String> enrolledList,
+    public EventModel(String facilityID,
                       Boolean geolocationRequired,
                       Integer capacity,
-                      String hashQR) {
+                      Date joinDeadline,
+                      String eventStrLocation,
+                      String eventTitle) {
         this.facilityID = facilityID;
         this.eventID = eventID;
-        this.waitingList = waitingList;
-        this.invitedList = invitedList;
-        this.cancelledList = cancelledList;
-        this.enrolledList = enrolledList;
         this.geolocationRequired = geolocationRequired;
         this.capacity = capacity;
-        this.hashQR = hashQR;
+        this.joinDeadline = joinDeadline;
+        this.eventStrLocation = eventStrLocation;
+        this.eventTitle = eventTitle;
+
+        this.waitingList = new ArrayList<String>();
+        this.invitedList = new ArrayList<String>();
+        this.cancelledList = new ArrayList<String>();
+        this.enrolledList = new ArrayList<String>();
     }
 
-    public String getFacilityID() {
-        return facilityID;
-    }
-
-    public void setFacilityID(String facilityID) {
-        this.facilityID = facilityID;
-    }
-
-    public String getEventID() {
-        return eventID;
-    }
-
-    public void setEventID(String eventID) {
+    public EventModel(String facilityID,
+                      String eventID,
+                      Boolean geolocationRequired,
+                      Integer capacity,
+                      Date joinDeadline,
+                      String eventStrLocation,
+                      String eventTitle) {
+        this(facilityID, geolocationRequired, capacity, joinDeadline, eventStrLocation, eventTitle);
         this.eventID = eventID;
     }
 
-    public ArrayList<String> getWaitingList() {
-        return waitingList;
+    /**
+     * This function adds another user to the waiting list only if the waiting list
+     * has not reached its limit, and if the user has not been added yet.
+     * @param userID The entrant's unique user ID
+     * @throws Exception Throws an exception if the waiting list is full
+     */
+    public void queueWaitingList(String userID) throws Exception {
+        if (waitingListIsFull()) {
+            throw new Exception("The waiting list for this event is full!");
+        }
+        if (!waitingList.contains(userID)) {
+            waitingList.add(userID);
+        }
     }
 
-
-    // TODO: add logic for adding/removing another person in a waiting list
-    public void setWaitingList(ArrayList<String> waitingList) {
-        this.waitingList = waitingList;
+    /**
+     * This function removes the provided user from the waiting list.
+     * @param userID The entrant's unique user ID
+     * @throws Exception Throws an exception if the user is not in the waiting list
+     */
+    public void unqueueWaitingList(String userID) throws Exception {
+        if (!waitingList.contains(userID)) {
+            throw new Exception("User has not joined the waiting list for this event yet!");
+        }
+        waitingList.remove(userID);
     }
 
-    public ArrayList<String> getInvitedList() {
-        return invitedList;
+    /**
+     * This function checks if the waiting list is full (if the waiting list has a limit).
+     * @return True if the waiting list has reached its limit; False otherwise
+     */
+    public Boolean waitingListIsFull() {
+        if (waitingListLimit == -1) {
+            return Boolean.FALSE;
+        }
+        return waitingList.size() == waitingListLimit;
     }
 
-    public void setInvitedList(ArrayList<String> invitedList) {
-        this.invitedList = invitedList;
+    // TODO: this function
+    public void queueInvitedList(String userID) {
+        return;
     }
 
-    public ArrayList<String> getCancelledList() {
-        return cancelledList;
+    // TODO: this function
+    public void unqueueInvitedList(String userID){
+        return;
     }
 
-    public void setCancelledList(ArrayList<String> cancelledList) {
-        this.cancelledList = cancelledList;
+    // TODO: this function
+    public void queueCancelledList(String userID) {
+        return;
     }
 
-    public ArrayList<String> getEnrolledList() {
-        return enrolledList;
+    // TODO: this function
+    public void queueEnrolledList(String userID) {
+        return;
     }
 
-    public void setEnrolledList(ArrayList<String> enrolledList) {
-        this.enrolledList = enrolledList;
-    }
-
-    public Boolean getGeolocationRequired() {
-        return geolocationRequired;
-    }
-
-    public void setGeolocationRequired(Boolean geolocationRequired) {
-        this.geolocationRequired = geolocationRequired;
-    }
-
-    public Integer getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(Integer capacity) {
-        this.capacity = capacity;
-    }
-
-    public String getHashQR() {
-        return hashQR;
-    }
-
-    public void setHashQR(String hashQR) {
-        this.hashQR = hashQR;
+    // TODO: customize to generate a unique ID to store as the event's document name in Firestore
+    private String generateID() {
+        Random rand = new Random();
+        return Integer.toString(rand.nextInt(100000));
     }
 }
