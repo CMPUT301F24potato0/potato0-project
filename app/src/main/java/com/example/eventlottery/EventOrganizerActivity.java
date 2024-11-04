742751a71132975527a505461c02dd998967d3df
Chirayu Shah <chirayushah61@gmail.com>
2024-11-04 14:31:21 -0700

    Event description not being updated in EventOrganizerActivity

--- app/src/main/java/com/example/eventlottery/EventOrganizerActivity.java
+++ app/src/main/java/com/example/eventlottery/EventOrganizerActivity.java
@@ -1,36 +1,116 @@
 package com.example.eventlottery;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class EventOrganizerActivity extends AppCompatActivity {
+    /*
+        Getting the views from the layout file
+     */
 
+    private FirebaseFirestore db;
+
+    private Button editEvent;
+    private FloatingActionButton back;
+    private TextView eventTitle;
+    private TextView eventDate;
+    private TextView eventDescription;
+    private ImageView eventPoster;
+    private TextView organizerName;
+    private Button QRCode;
+    private Button invited;
+    private Button cancelled;
     private Button waitlist;
+    private Button enrolled;
+    private LinearLayout eventView;
+
+    private String eventID;
+    private EventModel event;
+    private ConstraintLayout progessBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        db = FirebaseFirestore.getInstance();
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_event_organizer);
-
-        waitlist = (Button) findViewById(R.id.event_organizer_waitlist_button);
-        waitlist.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                event_waitlist eventWaitlist = new event_waitlist();
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.activity_event_organizer_id,eventWaitlist);
-                transaction.commit();
-            }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
 
+        /*
+            Initializing the views
+         */
+        editEvent = findViewById(R.id.event_organizer_edit_event_button);
+        back = findViewById(R.id.event_organizer_fab_button);
+        eventTitle = findViewById(R.id.event_organizer_event_title);
+        eventDate = findViewById(R.id.event_organizer_event_date);
+        eventDescription = findViewById(R.id.event_organizer_event_description);
+        eventPoster = findViewById(R.id.event_organizer_event_poster);
+        organizerName = findViewById(R.id.event_organizer_organizer_and_facility_names);
+        QRCode = findViewById(R.id.event_organizer_QR_code_view_button);
+        invited = findViewById(R.id.event_organizer_invited_button);
+        cancelled = findViewById(R.id.event_organizer_cancelled_button);
+        waitlist = findViewById(R.id.event_organizer_waitlist_button);
+        enrolled = findViewById(R.id.event_organizer_enrolled_button);
+        eventView = findViewById(R.id.event_view);
+
+        Bundle extra = getIntent().getExtras();
+        if (extra != null) {
+            eventID = extra.getString("event_id");
+        }
+        progessBar = findViewById(R.id.progressBar2);
+        db.collection("events").document(eventID)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                event = document.toObject(EventModel.class);
+                                eventTitle.setText(event.getEventTitle());
+                                eventDescription.setText(document.getString("description"));
+                                eventPoster.setImageResource(R.drawable.ic_facility_background);
+                                organizerName.setText(document.getString("organizer"));
+                                progessBar.setVisibility(View.GONE);
+                                eventView.setVisibility(View.VISIBLE);
+                            }
+                        }
+                        else {
+                            Log.d("Firebase error", "Cached get failed: ", task.getException());
+                        }
+                    }
+                });
+
+
+        QRCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new qr_code_dialog(eventID).show(getSupportFragmentManager(), "qr_code_dialog");
+            }
+        });
     }
 }
