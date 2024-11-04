312ea426d78c5194afc18c9280582d7d949e93b4
maxim-cher <maxcher5@gmail.com>
2024-11-04 13:14:54 -0700

    Opening waitlist fragment

--- app/src/main/java/com/example/eventlottery/EventOrganizerActivity.java
+++ app/src/main/java/com/example/eventlottery/EventOrganizerActivity.java
@@ -1,6 +1,8 @@
 package com.example.eventlottery;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class EventOrganizerActivity extends AppCompatActivity {
 
+    private Button waitlist;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_event_organizer);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        waitlist = (Button) findViewById(R.id.event_organizer_waitlist_button);
+        waitlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.activity_event_organizer_id,new event_waitlist());
+            }
         });
+
     }
 }
