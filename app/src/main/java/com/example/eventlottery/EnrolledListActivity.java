c174930453e222e02ed0a5c65f2ffad2560100e6
carrandez <vagundez1@gmail.com>
2024-11-05 12:37:22 -0700

    button from invited, cancelled and enrolled has functionality

--- app/src/main/java/com/example/eventlottery/EnrolledListActivity.java
+++ app/src/main/java/com/example/eventlottery/EnrolledListActivity.java
@@ -8,19 +8,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class InvitedListActivity extends AppCompatActivity {
+public class EnrolledListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_event_invited_list);
+        setContentView(R.layout.fragment_event_enroll_list);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
     }
 }
