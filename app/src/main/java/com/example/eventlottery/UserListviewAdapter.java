530a7f1b90168b064392c8140fe62be951d2a157
EdwardGallardo <gallardoedwardjason@gmail.com>
2024-11-05 13:39:22 -0700

    Implemented UserListviewAdapter

---
+++ app/src/main/java/com/example/eventlottery/UserListviewAdapter.java
@@ -0,0 +1,39 @@
+package com.example.eventlottery;
+
+import android.content.Context;
+import android.media.metrics.Event;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class UserListviewAdapter extends ArrayAdapter<UsersList> {
+
+    public UserListviewAdapter(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view;
+        if (convertView == null) {
+            view = View.inflate(getContext(), R.layout.user_listview_content, null);
+        }
+        else {
+            view = convertView;
+        }
+        UsersList user = getItem(position);
+        TextView userName = view.findViewById(R.id.listview_user_name);
+        userName.setText(user.getName());
+        Button sendInviteButton = view.findViewById(R.id.listview_send_invite_button);
+        Button removeButton = view.findViewById(R.id.listview_remove_button);
+        sendInviteButton.setVisibility(View.GONE);
+        removeButton.setVisibility(View.GONE);
+        return view;
+    }
+}
