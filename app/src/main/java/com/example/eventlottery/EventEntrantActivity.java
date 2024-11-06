0c928050c05e950c85bccd429c36010f073e2b8e
maxim-cher <maxcher5@gmail.com>
2024-11-05 22:36:46 -0700

    Moved notification permission to when the user joins event.

--- app/src/main/java/com/example/eventlottery/EventEntrantActivity.java
+++ app/src/main/java/com/example/eventlottery/EventEntrantActivity.java
@@ -1,6 +1,9 @@
 package com.example.eventlottery;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,8 +14,11 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -58,6 +64,40 @@ public class EventEntrantActivity extends AppCompatActivity {
         startActivity(i);
     }
 
+
+    // NEED TO TEST THIS
+    // ****************************************************************************************************************
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    // FCM SDK (and your app) can post notifications.
+                    Log.e("Permission","Granted");
+                } else {
+                    // TODO: Inform user that that your app will not show notifications.
+                    Log.e("Permission","Not granted");
+                }
+            });
+
+    private void askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                // FCM SDK (and your app) can post notifications.
+
+            } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
+            }
+        }
+    }
+    // ****************************************************************************************************************
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,6 +108,15 @@ public class EventEntrantActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+
+
+
+
+
+
 
         Bundle extras = getIntent().getExtras();
 
@@ -128,6 +177,7 @@ public class EventEntrantActivity extends AppCompatActivity {
                                 SubscribeToTopic subscribeToTopic = new SubscribeToTopic(topic,getApplicationContext());
                                 subscribeToTopic.subscribe();
                                 Log.e("Subscribed to topic",topic);
+                                askNotificationPermission();
 
                             }
                         });
