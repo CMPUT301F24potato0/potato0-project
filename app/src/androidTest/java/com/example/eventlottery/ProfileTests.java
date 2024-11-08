77c435f07804c18c2f184a9aa11567252497216b
Dzhan Maks He <dzhanmak@ualberta.ca>
2024-11-07 19:16:55 -0700

    Profile persistence tests

--- app/src/androidTest/java/com/example/eventlottery/ProfileTests.java
+++ app/src/androidTest/java/com/example/eventlottery/ProfileTests.java
@@ -1,14 +1,14 @@
 package com.example.eventlottery;
 
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
-import static org.hamcrest.CoreMatchers.any;
-import static org.hamcrest.CoreMatchers.is;
 
 import android.Manifest;
 
@@ -20,8 +20,6 @@ import androidx.test.rule.GrantPermissionRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import com.example.eventlottery.Waiter;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -38,5 +36,45 @@ public class ProfileTests {
     public void NavigateToProfile() {
         waiter.perform(withId(R.id.profile), click());
         onView(withId(R.id.fProfile)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void ProfileEditPersistent() {
+        NavigateToProfile();
+        waiter.perform(withId(R.id.fNameEditText), replaceText("Persistence tester first name"));
+        onView(withId(R.id.lNameEditText)).perform(replaceText("Persistence tester last name"));
+        onView(withId(R.id.phoneEditText)).perform(replaceText("000 000 000 111"));
+        onView(withId(R.id.emailEditText)).perform(replaceText("persist@example.com"));
+        closeSoftKeyboard();
+        waiter.perform(withId(R.id.saveProfileBtn), click());
+        onView(withId(R.id.facility)).perform(click());
+        onView(withId(R.id.saveProfileBtn)).check(doesNotExist());
+        onView(withId(R.id.profile)).perform(click());
+        waiter.check(withId(R.id.fNameEditText), matches(withText("Persistence tester first name")));
+        onView(withId(R.id.lNameEditText)).check(matches(withText("Persistence tester last name")));
+        onView(withId(R.id.phoneEditText)).check(matches(withText("000 000 000 111")));
+        onView(withId(R.id.emailEditText)).check(matches(withText("persist@example.com")));
+        waiter.perform(withId(R.id.fNameEditText), replaceText("Tester first name"));
+        onView(withId(R.id.lNameEditText)).perform(replaceText("Tester last name"));
+        onView(withId(R.id.phoneEditText)).perform(replaceText("000 000 000 000"));
+        onView(withId(R.id.emailEditText)).perform(replaceText("tester@example.com"));
+        closeSoftKeyboard();
+        waiter.perform(withId(R.id.saveProfileBtn), click());
+    }
+    @Test
+    public void ProfileEditDiscardUnsaved() {
+        ProfileEditPersistent();
+        waiter.perform(withId(R.id.fNameEditText), replaceText("Temporary first name"));
+        onView(withId(R.id.lNameEditText)).perform(replaceText("Temporary last name"));
+        onView(withId(R.id.phoneEditText)).perform(replaceText("111 111 111 111"));
+        onView(withId(R.id.emailEditText)).perform(replaceText("temp@example.com"));
+        closeSoftKeyboard();
+        waiter.perform(withId(R.id.facility), click());
+        onView(withId(R.id.saveProfileBtn)).check(doesNotExist());
+        onView(withId(R.id.profile)).perform(click());
+        waiter.check(withId(R.id.fNameEditText), matches(withText("Tester first name")));
+        onView(withId(R.id.lNameEditText)).check(matches(withText("Tester last name")));
+        onView(withId(R.id.phoneEditText)).check(matches(withText("000 000 000 000")));
+        onView(withId(R.id.emailEditText)).check(matches(withText("tester@example.com")));
     }
 }
