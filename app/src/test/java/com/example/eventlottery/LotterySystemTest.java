b216ec3aaa96d295157dafb8ed0b5c045bf8f119
Dzhan Maks He <dzhanmak@ualberta.ca>
2024-11-08 14:33:01 -0700

    Unit test for lottery sample drawing

---
+++ app/src/test/java/com/example/eventlottery/LotterySystemTest.java
@@ -0,0 +1,19 @@
+package com.example.eventlottery;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class LotterySystemTest {
+    @Test
+    public void drawSampleTest() {
+        ArrayList<UsersList> userList = new ArrayList<>();
+        for (int i = 0; i < 15; i++) {
+            userList.add(new UsersList("0", "Name"));
+        }
+        ArrayList<UsersList> drawnList = LotterySystem.sampleEntrants(userList, 5);
+        assertEquals(drawnList.size(), 5);
+    }
+}
