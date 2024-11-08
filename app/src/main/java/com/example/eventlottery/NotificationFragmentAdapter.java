11e8146c6a7792cc0cf05a8217a4c0ca31aded85
maxim-cher <maxcher5@gmail.com>
2024-11-08 08:54:23 -0700

    NotificationFragmentAdapter.

--- app/src/main/java/com/example/eventlottery/NotificationFragmentAdapter.java
+++ app/src/main/java/com/example/eventlottery/NotificationFragmentAdapter.java
@@ -5,16 +5,85 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-public class NotificationFragmentAdapter extends ArrayAdapter {
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class NotificationFragmentAdapter extends ArrayAdapter<CurrentUser> {
+
+
+    private CurrentUser currentUser;
+    private FirebaseFirestore db;
 
     public NotificationFragmentAdapter(@NonNull Context context, int resource) {
         super(context, resource);
     }
 
+    public NotificationFragmentAdapter(@NonNull Context context, int resource, CurrentUser currentUser, FirebaseFirestore db) {
+        super(context, resource);
+        this.currentUser = currentUser;
+        this.db = db;
+    }
 
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//        return super.getView(position, convertView, parent);
+        View view;
+        if (convertView == null) {
+            view = View.inflate(getContext(), R.layout.notification_adapter_view, null);
+        } else {
+            view = convertView;
+        }
+
+        // get the notification from the data base using db and curUser
+        TextView title = (TextView) view.findViewById(R.id.title_id);
+        TextView message = (TextView) view.findViewById(R.id.message_id);
+        TextView eventID = (TextView) view.findViewById(R.id.envent_id);
+        TextView flagID = (TextView) view.findViewById(R.id.flag_id);
+        Button cancel_btn = (Button) view.findViewById(R.id.cancel_id);
+        Button signup_btn = (Button) view.findViewById(R.id.signup_id);
+        CurrentUser user = getItem(position);
+        ArrayList<HashMap<String, String>> notification = user.getNotifications();
+        title.setText((CharSequence) notification.get(Integer.parseInt("title")));
+        message.setText((CharSequence) notification.get(Integer.parseInt("body")));
+        eventID.setText((CharSequence) notification.get(Integer.parseInt("eventID")));
+        flagID.setText((CharSequence) notification.get(Integer.parseInt("flag")));
+
+        cancel_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cancel_btn.setVisibility(View.GONE);
+                signup_btn.setVisibility(View.GONE);
+
+            }
+        });
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cancel_btn.setVisibility(View.GONE);
+                signup_btn.setVisibility(View.GONE);
+                // remove from invited
+                // move to enrolled
+            }
+        });
+
+
+
+
+
+        // if flag "chose" then button visible
+        // button will add user to enrolled, remove from invited, remove button and update data base
+        // if button cancel then remove both buttons, move user to canceled list and remove from invited
+
+
+        return view;
+    }
 }
--- app/src/main/java/com/example/eventlottery/NotificationsFragment.java
+++ app/src/main/java/com/example/eventlottery/NotificationsFragment.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 /**
  * This class is the Notification fragment
@@ -15,15 +18,21 @@ import androidx.recyclerview.widget.RecyclerView;
  */
 public class NotificationsFragment extends Fragment {
 
+    private FirebaseFirestore db;
     private CurrentUser curUser;
+;
+    private ListView notification_listview;
 
     public NotificationsFragment(){
         // require a empty public constructor
     }
 
-    public NotificationsFragment(CurrentUser cu) {
-        this.curUser = cu;
+    public NotificationsFragment(FirebaseFirestore db, CurrentUser curUser) {
+        this.db = db;
+        this.curUser = curUser;
     }
+
+
 
     /**
      *
@@ -40,9 +49,24 @@ public class NotificationsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        notification_listview = view.findViewById(R.id.notification_listview);
+        NotificationFragmentAdapter adapter = new NotificationFragmentAdapter(
+                getContext(),
+                100,
+                curUser,
+                db
+        );
+//        NotificationFragmentAdapter adapter = new NotificationFragmentAdapter(
+//                getContext(),
+//                100
+//        );
+        notification_listview.setAdapter(adapter);
 
 
 
-        return inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        return view;
     }
 }
