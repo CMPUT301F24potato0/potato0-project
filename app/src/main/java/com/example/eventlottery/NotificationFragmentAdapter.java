7fdb924a2f7c8f1035b11676f65441b0468256ed
maxim-cher <maxcher5@gmail.com>
2024-11-07 20:00:57 -0700

    Update UI + created NotificationFragmentAdapter.

---
+++ app/src/main/java/com/example/eventlottery/NotificationFragmentAdapter.java
@@ -0,0 +1,20 @@
+package com.example.eventlottery;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class NotificationFragmentAdapter extends ArrayAdapter {
+
+
+    public NotificationFragmentAdapter(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
+
+
+}
