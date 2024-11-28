7c700b665176f38f2667fb122f30890fd55a2f93
maxim-cher <maxcher5@gmail.com>
2024-11-28 13:21:15 -0700

    Add back button, code clean up.

--- app/src/main/java/com/example/eventlottery/Notifications/PushNotificationService.java
+++ app/src/main/java/com/example/eventlottery/Notifications/PushNotificationService.java
@@ -73,19 +73,6 @@ public class PushNotificationService extends FirebaseMessagingService{
                     .setStyle(new NotificationCompat.BigTextStyle())
                     .setAutoCancel(true);
 
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-
-
 
             if(!ismuted) {
                 Log.e("Ismuted??", ""+ false);
