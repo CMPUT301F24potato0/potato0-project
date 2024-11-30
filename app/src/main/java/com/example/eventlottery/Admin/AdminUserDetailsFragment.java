ed0e3a958ac8f1e699f89e39bf5600701ce8019c
Dzhan Maks He <dzhanmak@ualberta.ca>
2024-11-30 12:43:45 -0700

    Make admin can't delete themselves

--- app/src/main/java/com/example/eventlottery/Admin/AdminUserDetailsFragment.java
+++ app/src/main/java/com/example/eventlottery/Admin/AdminUserDetailsFragment.java
@@ -14,12 +14,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.DialogFragment;
 
+import com.example.eventlottery.MainActivity;
 import com.example.eventlottery.Models.UserModel;
 import com.example.eventlottery.R;
 import com.google.firebase.firestore.Blob;
@@ -29,6 +31,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.io.ByteArrayOutputStream;
 import java.util.Random;
+
+import java.util.Objects;
 
 public class AdminUserDetailsFragment extends DialogFragment {
     private final UserModel user;
@@ -51,7 +55,10 @@ public class AdminUserDetailsFragment extends DialogFragment {
         ((TextView)rootView.findViewById(R.id.admin_phone_info)).setText(String.format("%s", user.getPhone()));
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         rootView.findViewById(R.id.admin_delete_user).setOnClickListener((View view) -> {
-            user.delete(db);
+            if (!Objects.equals(user.getiD(), MainActivity.curUser.getiD()))
+                user.delete(db);
+            else
+                Toast.makeText(getContext(), "Can't delete yourself", Toast.LENGTH_SHORT).show();
             dismiss();
         });
         rootView.findViewById(R.id.admin_user_cancel_button).setOnClickListener((View view) -> {
